5d0eac13559e9a5865ceb3cfa130381aa43cf511
MatiasDonati <matod87@hotmail.com>
2022-02-13 09:47:25 -0300

    dash

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -56,7 +56,7 @@ class ContentRowTop extends Component {
 				{/*<!-- Content Row Top -->*/}
 				<div className="container-fluid">
 					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">WS App Dashboard</h1>
+						<h1 className="h3 mb-0 text-gray-800"></h1>
 					</div>
 				
 					{/*<!-- Content Row Movies-->*/}
